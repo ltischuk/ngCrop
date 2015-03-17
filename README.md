40e5f66675c58d5b9a89f361604e059b8c1b572c
ltischuk <ltischuk@accesso.com>
2015-03-16 22:12:13 -0400

    added additional unit tests and README scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ngCrop
+
+## Overview
+ngCrop is an AngularJS module that provides a directive for image cropping within web browsers using the HTML5 canvas element.
+The current version only works on Desktop browsers with Mobile support slated for the future.
+
+## Requirements
+
+* AngularJS 1.2.x (Tested with 1.2.28)
+* HTML5 supported browsers
+
+## Usage
+
--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,17 @@ module.exports = function(config) {
   config.set({
     autoWatch : false,
 
+    basePath: '',
+
     frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/ngcrop/ngcrop.js',
+      'src/ngcrop/*.js',
+      'src/ngcrop/test/**/*.spec.js'
+    ],
 
     browsers : ['PhantomJS'],
 
